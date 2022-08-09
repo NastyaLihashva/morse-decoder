4a641db2f87e86117395b97e13ba371815fed77d
NastyaLihashva <kassiopea308@mail.ru>
2022-08-09 21:19:58 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -38,8 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodeNumbers = [];
+    let result = '';
+    for(let i = 0; i < expr.length; i += 10){
+        let str = '';
+        for(let j = 0; j < 10; j+=2){
+            if(expr[i+j]+expr[i+j+1] === '10'){
+                str += '.';
+            }
+            if(expr[i+j]+expr[i+j+1] === '11'){
+                str += '-';
+            }
+            if(expr[i+j] === '*'){
+                str += ' ';
+            }
+        }
+        decodeNumbers.push(str);
+    }
+    for(let key in decodeNumbers){
+        if(decodeNumbers[key] === '     '){
+            result += ' ';
+        }
+        else{
+            result += MORSE_TABLE[decodeNumbers[key]];
+        }
+    }
+
+    return result;
 }
+
 
 module.exports = {
     decode
